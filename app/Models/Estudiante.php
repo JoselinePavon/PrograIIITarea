aecb6eacb48c5cd6e564eb81d73ab88f7484831c
JoselinePavon <52256066+JoselinePavon@users.noreply.github.com>
2022-03-03 14:58:14 -0600

    CrudStudent

---
+++ app/Models/Estudiante.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Estudiante extends Model
+{
+    protected $table='estudiantes';
+    public $timestamps=false;
+    protected $fillable=[
+        'id', 'nombre', 'apellido', 'correo', 'carrera', 'semestre', 'carne'
+    ];
+
+    protected $primaryKey='id';
+}
